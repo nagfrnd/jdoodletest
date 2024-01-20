b99b23f313fc8a895731b189082023b11e10c876
nagfrnd <139296185+nagfrnd@users.noreply.github.com>
2024-01-20 22:52:54 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,17 +3,17 @@ variable "aws_region" {
 }
 
 variable "vpc_id" {
-    default = "vpc-072d157fac6d56872" # default VPC
+    default = "" # Please update VPC
 }
 
 variable "subnet1" {
-    default = "subnet-0a630641a7d1e3dc6" # us-east-1a
+    default = "" # Please update
 }
 
 variable "subnet2" {
-    default = "subnet-09962f19d46766153" # us-east-1b
+    default = "" # # Please update
 }
 
 variable "subnet3" {
-    default = "subnet-08ac1b2b8ef09fa40" # us-east-1f
+    default = "" # # Please update
 }
